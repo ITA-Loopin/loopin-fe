1ae7066ee42d6172fc9cb89bae2a05efe0223222
chaewon41 <chaew0m3x@gmail.com>
2025-12-30 01:39:58 +0900

    style: 루프 추가하기 버튼 크기 조정

--- src/components/calendar/AddLoopButton.tsx
+++ src/components/calendar/AddLoopButton.tsx
@@ -7,7 +7,7 @@ export function AddLoopButton({ onClick }: AddLoopButtonProps) {
     <button
       type="button"
       onClick={onClick}
-      className="flex w-full max-w-[500px] h-12 py-[15px] px-[121px] justify-center items-center gap-[10px] self-stretch rounded-[30px] bg-[var(--gray-800,#3A3D40)] text-base font-semibold leading-[150%] tracking-[-0.32px] text-[var(--gray-white,#FFF)]"
+      className="flex w-full h-12 py-[15px] px-[121px] justify-center items-center gap-[10px] rounded-[30px] bg-[var(--gray-800,#3A3D40)] text-base font-semibold leading-[150%] tracking-[-0.32px] text-[var(--gray-white,#FFF)]"
     >
       루프 추가하기
     </button>
