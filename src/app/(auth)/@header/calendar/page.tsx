e43726bc1d5a185a06552c39acea6f5f4fa5ed37
LEE JEONGHAN <ljhh1648@gmail.com>
2026-05-24 18:21:48 +0900

    introduce page

---
+++ src/app/(auth)/@header/calendar/page.tsx
@@ -0,0 +1,5 @@
+import Header from "@/components/common/Header";
+
+export default function CalendarHeader() {
+  return <Header />;
+}
---
+++ src/app/(auth)/@header/my-page/profile-edit/page.tsx
@@ -0,0 +1,5 @@
+import Header from "@/components/common/Header";
+
+export default function MyPageProfileEditHeader() {
+  return <Header leftType="back" rightType="none" centerTitle="프로필 편집" />;
+}
